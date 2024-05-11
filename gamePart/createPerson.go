a24318ab5bc3b0ff20c1bba6c323262aa2e2025e
Vitaly Surkov <vitaliksy@mail.ru>
2024-05-11 14:46:51 +0300

    creat files start

---
+++ gamePart/createPerson.go
@@ -0,0 +1,5 @@
+package gamePart
+
+func СreatePerson() {
+
+}
--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"./assistant"
+)
+
+func main() {
+	fmt.Println(assistant.Hi)
+
+	// var name string
+	// var age int
+	// fmt.Print("Введите имя: ")
+	// fmt.Fscan(os.Stdin, &name)
+
+	// fmt.Print("Введите возраст: ")
+	// fmt.Fscan(os.Stdin, &age)
+
+	// fmt.Println(name, age)
 }
