d5ec3856e87ed2c88902a0ab64837ac5b828b4a9
Ilya <lsurkovl@mail.ru>
2024-05-12 22:57:05 +0300

    stat person

--- assistant/listProf.go
+++ assistant/listProf.go
@@ -13,5 +13,5 @@ type Specifications struct {
 }
 
 var Strongman = Prof{Name: "Силач", Specifications: Specifications{Hp: 980, Mana: 350, Str: 18, Agil: 7, Inelect: 7}}
-var Wizard = Prof{Name: "Волшебник", Specifications: Specifications{Hp: 980, Mana: 350, Str: 18, Agil: 7, Inelect: 7}}
-var Agilman = Prof{Name: "Ловкач", Specifications: Specifications{Hp: 980, Mana: 350, Str: 18, Agil: 7, Inelect: 7}}
+var Wizard = Prof{Name: "Волшебник", Specifications: Specifications{Hp: 800, Mana: 700, Str: 10, Agil: 9, Inelect: 20}}
+var Agilman = Prof{Name: "Ловкач", Specifications: Specifications{Hp: 600, Mana: 500, Str: 8, Agil: 22, Inelect: 12}}
